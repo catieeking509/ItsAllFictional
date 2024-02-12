d0126146bde69afef991c1d87b012f59e02df834
Catie K. <137359327+catieeking509@users.noreply.github.com>
2024-02-12 16:41:21 -0600

    data.json and bookshlef

--- src/App.js
+++ src/App.js
@@ -1,57 +1,21 @@
-import { useState, useEffect } from 'react';
-import { nanoid } from "nanoid";
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import BookShelf from './Components/BookShelf.jsx';
 import Home from './Components/Home.jsx';
 import NavBar from './Components/NavBar.jsx';
-import BookShelf from './Components/BookShelf.jsx';
+
 import Favorites from './Components/Favorites.jsx';
 import ContactForm from './Components/Contact.jsx';
 import LoginPage from './Components/LoginPage.jsx';
-import BookForm from './Components/Book_Admin.jsx';
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 
 
-
-
 function App() {
-
-  let [Books, setBooks] = useState([]);
-
-
-
-  function initializePageState() {
-    let savedState = localStorage.getItem("items");
-    if (typeof savedState === 'string') {
-      let parsedState = JSON.parse(savedState);
-      setBooks(parsedState);
-    };
-  }
-
-  useEffect(initializePageState, []);
-
-  function addItem(date, link, description, rating, title, author) {
-    setBooks(function (oldItems) {
-      let newItems = [
-        ...oldItems, 
-        {
-          date,
-          description,
-          link,
-          rating,
-          title,
-          author,
-          id: nanoid(),
-        },
-      ];
-      localStorage.setItem('items', JSON.stringify(newItems));
-      return newItems;
-    });
-  }
-
 
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
@@ -62,11 +26,10 @@ function App() {
         <main>
           <Routes>
             <Route exact="true" path="/" element={<Home />} />
-            <Route path="/BookShelf" element={<BookShelf Books={Books} />} />
+            <Route path="/BookShelf" element={<BookShelf />} />
             <Route path="/Favorites" element={<Favorites />} />
             <Route path="/Contact" element={<ContactForm />} />
             <Route path="/LoginPage" element={<LoginPage />} />
-            <Route path="/Book_Admin" element={<BookForm submitItem={addItem} />} />
           </Routes>
         </main>
         <footer className="footer">
@@ -77,6 +40,5 @@ function App() {
 
   );
 }
-
 export default App;
 
--- src/Components/BookShelf.jsx
+++ src/Components/BookShelf.jsx
@@ -1,23 +1,49 @@
-import React from 'react';
-import Book from './Book.jsx';
-
+import React, { useEffect, useState } from 'react';
+import { Card, CardHeader, CardBody, CardTitle, CardText } from 'reactstrap';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import '../App.css';
 
 
-export default function BookShelf( {Books} ) {
+export default function BookShelf() {
 
+  const [books, setBooks] = useState([]);
 
-  let BooksJsx = Books.map ( book => 
-    <Book key= {book.id} {...book} />
-  );
+  useEffect(() => {
+    axios.get('./2024.data.json')
+    .then((response) => {
+      setBooks(response.data.books);
+    });
+  }, [books]);
 
-  
 
   return (
-    <section className="bookshelf">
-      { BooksJsx }
-    </section>
+  <div className="books-section">
+    {books&&books.map((book) => (
+      <Card
+        key= {book.ibsn}
+        className="my-2 card"
+        inverse
+        style={{
+        width: 'auto'
+        }}
+      >
+        <CardHeader className="card-header">
+            {book.title}
+        </CardHeader>
+        <CardBody>
+          <CardTitle tag="h5">
+              {book.author}
+          </CardTitle>
+          <CardText>
+            <>{book.description}</>
+            <>{book.rating}</>
+            <>{book.opinion}</> 
+          </CardText>
+        </CardBody> 
+      </Card>
+    ))};
+  </div>
+
   );
 }
